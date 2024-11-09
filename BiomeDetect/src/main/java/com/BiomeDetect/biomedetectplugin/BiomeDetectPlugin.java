a7328e74793aa0fa4d2731953424fc5c73702a43
AJ Young <albertjyoung1@gmail.com>
2024-11-09 02:41:55 -0500

    new file:   BiomeDetect/.gitignore 	new file:   BiomeDetect/.idea/.gitignore 	new file:   BiomeDetect/.idea/encodings.xml 	new file:   BiomeDetect/.idea/misc.xml 	new file:   BiomeDetect/pom.xml 	new file:   BiomeDetect/src/main/java/com/BiomeDetect/biomedetectplugin/BiomeDetectPlugin.java 	new file:   BiomeDetect/src/main/java/org/example/Main.java 	new file:   BiomeDetect/src/main/resources/plugin.yml

---
+++ BiomeDetect/src/main/java/com/BiomeDetect/biomedetectplugin/BiomeDetectPlugin.java
@@ -0,0 +1,10 @@
+package com.BiomeDetect.biomedetectplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BiomeDetectPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("Yo bro, my awesome plugin is indeed working!");
+    }
+}
